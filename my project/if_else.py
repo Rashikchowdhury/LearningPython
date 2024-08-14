c6cdbef452dbacc9fed20d8d519a0cc08c4d5b53
Rashikchowdhury <muhammadrashikchowdhury@gmail.com>
2024-08-14 10:42:24 +0600

    python file updated

--- my project/if_else.py
+++ my project/if_else.py
@@ -1,6 +1,12 @@
 a= 10
-b= 20
-if a>b:
-    print("A is greater than B")
-else:
-    print("A is less then B")
+b= 10
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("a is less than b")
+elif a == b:
+    print("a is equal b")
+elif a != b:
+    print("a is not equal b")
+elif a >= b:
+    print("a is greater than b or equal b")
